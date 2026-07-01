fcd3c0ff45da82d827a33bae93d4d5b33ba91380
dharu-dharanic <dharanic5651@gmail.com>
2026-07-01 11:10:55 +0530

    Update frontend API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://reportit-backend.onrender.com/api'
 });
 
 // Automatically add token to every request
